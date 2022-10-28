b1aae50e19da6aa4405ad9f2d1ff22c7f669579f
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2022-10-28 16:52:21 -0300

    função para entrar na sala implementada

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let time; let from; let to; let text; let userStatus;
+
+
 const message = `<mensagem>${time} <b>${from}</b> para <b>${to}</b> ${text}</mensagem>`
 const status = `<mensagem>${time} <b>${from}</b> entrou na sala...</mensagem>`
 
@@ -5,4 +8,34 @@ function html(){
     document.querySelector("header").innerHTML = ` <img src="./img/logo 1.png" alt=""> <ion-icon  name="people-outline"></ion-icon>`
     document.querySelector("footer").innerHTML =  `<input type="text" placeholder="Escreva aqui..."> <ion-icon name="paper-plane-outline"></ion-icon>`   
 }
+
 html()
+
+function statusCode(promisse){
+    const userStatus = promisse.status;
+    return userStatus
+}
+
+function pedirNome (){
+    const nome = {
+        name: prompt("Digite um nome de usuário:")
+    }
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nome)
+    promise.catch(function(error){
+        // Em caso de erros
+        console.log(error);
+        alert("Usuário inválido, tente outro")
+        pedirNome()
+    });
+    promise.then(function(response){
+        //Em caso de sucesso
+        console.log(response);
+    })
+}
+
+function entrarSala(){
+    pedirNome()
+}
+function atualizaMensagens(){
+    axios.get("")
+}
