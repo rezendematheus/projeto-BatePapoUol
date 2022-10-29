41e1c7fc15053a46a00db1a77848f14c56a29ed5
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2022-10-29 00:01:30 -0300

    Busca e atualização de mensagens, layout mensagens

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let time; let from; let to; let text; let userStatus; let nome; let count = 0;
+let type; let time; let from; let to; let text; let userStatus; let nome; let data;
+const message = `<${type}>${time} <span>${from}</span> para <span>${to}</span> ${text}</${type}>`
+const status = `<${type}>${time} <span>${from}</span> entrou na sala...</${type}>`
 
-
-const message = `<mensagem>${time} <b>${from}</b> para <b>${to}</b> ${text}</mensagem>`
-const status = `<mensagem>${time} <b>${from}</b> entrou na sala...</mensagem>`
+pedirNome();    
 
 function html(){
     document.querySelector("header").innerHTML = ` <img src="./img/logo 1.png" alt=""> <ion-icon  name="people-outline"></ion-icon>`
@@ -16,6 +16,27 @@ function mandaStatus(){
     
 }
 
+function buscaMensagens(){
+    document.querySelector("chat").innerHTML =""
+    const response = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    response.then(function(response){
+        data = response.data
+        data.forEach((element) => {
+            
+            const mensagem = document.querySelector("chat").innerHTML += `<${element.type}><p><time>(${element.time})</time> <span>${element.from}</span> para <span>${element.to}</span> ${element.text}</p></${element.type}>`;
+            
+        })
+        document.querySelector("chat").lastChild.scrollIntoView()
+    },
+    )
+
+    
+    response.catch(function(error){
+        console.log(error)
+    })
+}
+
+
 function pedirNome (){
     nome = {
         name: prompt("Digite um nome de usuário:")
@@ -24,26 +45,16 @@ function pedirNome (){
     promise.catch(function(error){
         // Em caso de erros
         console.log(error);
-        if(error.status === 400){
-            alert("Usuário inválido, tente outro")
-            pedirNome()
-        }
-        else{
-            alert("Algo deu errado, atualize a página e tente novamente")
-        }
+        alert("Usuário inválido, tente outro");
+        pedirNome();
+
     });
     promise.then(function(response){
         //Em caso de sucesso
         console.log(response);
+        setInterval(mandaStatus, 4000);
+        buscaMensagens();
+        setInterval(buscaMensagens, 5000, response)
     })
-    setInterval(mandaStatus, 4000);
 }
 
-
-
-function entrarSala(){
-    pedirNome()
-}
-function atualizaMensagens(){
-    axios.get("")
-}
